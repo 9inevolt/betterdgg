fa0c1fbb6bb5a33210f0124d26ed6cf729811195
mellipelli <mellipelliasldkjfdjfkdjf3r8w@mailinator.com>
2014-10-18 01:52:53 -0400

    Add /multi command for stalking multiple people
    This adds the /multi command to the client, which the backend now supports. I followed all the same conventions you did in your version of /stalk.

--- betterdgg/modules/stalk.js
+++ betterdgg/modules/stalk.js
@@ -50,38 +50,70 @@
 
         return {
             init: function() {
-                // hook into handle command
-                var fnHandleCommand = destiny.chat.handleCommand;
-                destiny.chat.handleCommand = function(str) {
-                    var sendstr = str.trim();
-                    var Regexp = /^s(?:talk)?\s+([\w\d]+)\s*(\d+)?/g;  //matches format $stalk {name} {number of messages to stalk}
-                    var match = Regexp.exec(sendstr);
-                    var number;
-                    
-                    if (match) {
-                      var sock = MySocket();
-                      sock.onopen = function() {
-                          sock.send(   //send stalk query to backend
-                            JSON.stringify({
-                               "Session" :   destiny.chat.user.username,
-                               "QueryType" : "s",
-                               "Name" :      match[1],
-                              "Number" :    ((typeof match[2] === 'undefined')? "3" :match[2])  //default to 3 lines if unspecified.
-                            }));
-                            setTimeout(function() {
-                                if (sock.readyState < 2) {
-                                    PushChat("Timed out stalking " + match[1]);
-                                    sock.close();
-                                }
-                            }, 5000);
-                      };
-                    } else if (sendstr.match(/^s(?:talk)?\s*$/)) {
-                        PushChat("Command not understood. Format: /stalk {username} #");
-                        PushChat("# optional, /stalk alias: /s")
-                    } else {
-                      fnHandleCommand.apply(this, arguments);
-                    }
-                };
+              // hook into handle command
+              var fnHandleCommand = destiny.chat.handleCommand;
+              destiny.chat.handleCommand = function(str) {
+                  var sendstr = str.trim();
+                  if (match = sendstr.match(/^s(?:talk)?\s+([\w\d]+)\s*(\d+)?/)) { //matches format /stalk {name} {number of messages to stalk}
+                  var sock = MySocket();
+                  sock.onopen = function() {
+                      sock.send( //send stalk query to backend
+                      JSON.stringify({
+                      "Session" : destiny.chat.user.username,
+                      "QueryType" : "s",
+                      "Name" : match[1],
+                      "Number" : ((typeof match[2] === 'undefined')? "3" :match[2]) //default to 3 lines if unspecified.
+                  }));
+                  setTimeout(function() {
+                      if (sock.readyState < 2) {
+                      PushChat("Timed out stalking " + match[1]);
+                      sock.close();
+                      }
+                    }, 5000);
+                  };
+                  } else if (sendstr.match(/^s(?:talk)?\s*$/)) {
+                    PushChat("Command not understood. Format: /stalk {username} #");
+                    PushChat("# optional, /stalk alias: /s");
+                  } else if (match = sendstr.match(/^m(?:ulti)?\s+([\w\d]+)\s*([\w\d]+)?\s*([\w\d]+)?\s*([\w\d]+)?\s*(\d{1,3})?\s*$/)) { //matches format /multi {name}(up to 4) #
+                        var querystr = {"Session": destiny.chat.user.username,
+                                        "QueryType": "m",
+                                        };
+                        for (var i = 1; match[i] !== undefined; i++);
+                
+                        var Names = [];
+                        if (/^\d+$/.test(match[i-1])) {  //if last argument is integer, set n. otherwise, n is default (10)
+                          n = match[i-1];
+                        } else {
+                          n = "10";
+                          Names.push(match[i-1]);
+                        }
+                
+                        for (var j = 1; j < i-1; j++) {
+                          Names.push(match[j]);
+                        }
+                
+                        querystr["Names"] = Names;
+                        querystr["Number"] = n;
+                        
+                        var sock = MySocket();
+                        sock.onopen = function() {
+                          sock.send( //send stalk query to backend
+                            JSON.stringify(querystr));
+                        setTimeout(function() {
+                            if (sock.readyState < 2) {
+                            PushChat("Timed out stalking multiple people.");
+                            sock.close();
+                            }
+                          }, 5000);
+                        };
+                  } else if (sendstr.match(/^m(?:ulti)?\s*$/)) {
+                    PushChat("Command not understood.");
+                    PushChat("Format: /multi {username} {username} #");
+                    PushChat("up to 4 usernames, # optional, alias: /m");
+                  } else {
+                    fnHandleCommand.apply(this, arguments);
+                  } 
+              };
             }
         }
     })();
