9d295193caac97fa7ee2e0d94fba3d3028afd3d5
9inevolt <9inevolt@twitch.tv>
2015-03-04 20:45:00 -0800

    fix broken flair. Fixes #61

--- betterdgg/modules/flair.js
+++ betterdgg/modules/flair.js
@@ -3,15 +3,15 @@
 
     bdgg.flair = (function() {
         destiny.UserFeatures['BDGG_CONTRIBUTOR'] = 'bdgg_contributor';
-        var fnGetFeatureHTML = ChatUser.prototype.getFeatureHTML;
+        var fnGetFeatureHTML = ChatUserMessage.prototype.getFeatureHTML;
         var bdggGetFeatureHTML = function() {
             var icons = fnGetFeatureHTML.apply(this, arguments);
-            if (CONTRIBUTORS.indexOf(this.username) > -1) {
+            if (CONTRIBUTORS.indexOf(this.user.username) > -1) {
                 icons += '<i class="icon-bdgg-contributor" title="Better Destiny.gg Contributor"/>';
             }
             return icons;
         };
-        ChatUser.prototype.getFeatureHTML = bdggGetFeatureHTML;
+        ChatUserMessage.prototype.getFeatureHTML = bdggGetFeatureHTML;
         return {};
     })();
 }(window.BetterDGG = window.BetterDGG || {}));
