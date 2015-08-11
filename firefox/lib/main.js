42c92edb90e2dca611428172a6b3119c281f8675
9inevolt <9inevolt@twitch.tv>
2015-08-11 12:14:17 -0700

    did i forget to commit this fix for firefox loadScript?

--- firefox/lib/main.js
+++ firefox/lib/main.js
@@ -2,6 +2,35 @@ const winUtils = require("sdk/window/utils");
 const { ToggleButton } = require("sdk/ui/button/toggle");
 const windowListener = require("./window-listener.js");
 
+const { Cc, Ci, components } = require("chrome");
+//var instance = Cc["@mozilla.org/moz/jssubscript-loader;1"];
+//var loader = instance.getService(Ci.mozIJSSubScriptLoader);
+//function loadScript(url) {
+//    loader.loadSubScript(url);
+//}
+//
+//loadScript('resource://betterdgg-at-destiny-dot-gg/betterdgg/lib/jsbn.js');
+//require('./jsbn.js');
+
+function loadScript(name, context) {
+    // Create the Sandbox
+    let sandbox = components.utils.Sandbox(context, {
+        sandboxPrototype: context,
+        wantXrays: false
+    });
+
+    // Get the caller's filename
+    let file = components.caller.stack.filename;
+    // Strip off any prefixes added by the sub-script loader
+    // and the trailing filename
+    let directory = file.replace(/.* -> |[^\/]+$/g, "");
+
+    Services.scriptloader.loadSubScript(directory + name,
+                                        sandbox, "UTF-8");
+}
+
+loadScript("jsbn.js", this);
+
 var button = ToggleButton({
     id: "sidebar_toggle_button",
     label: "Sidebar",
