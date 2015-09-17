74aa4775ad8773be1931a271ba552ff0a04228b2
9inevolt <9inevolt@twitch.tv>
2015-09-17 16:07:23 -0700

    add test push method to protect against pushing null/undefined to chat

--- betterdgg/modules/test.js
+++ betterdgg/modules/test.js
@@ -1,9 +1,13 @@
 ;(function(bdgg) {
+    function push(msg) {
+        msg && destiny.chat.gui.push(msg);
+    };
+
     bdgg.test = { chat: {}, or: {} },
     bdgg.test1 = function(msg) {
         var m = destiny.chat.onMSG({data:msg || '>BetterDGG DESBRO ASLAN DJAslan LUL LUL LUL',
             nick:'BetterDGG', features:[]});
-        destiny.chat.gui.push(m);
+        push(m);
         setTimeout(function() { destiny.chat.onMUTE({data: 'BetterDGG'}) }, 3000);
     };
 
@@ -14,19 +18,19 @@
         destiny.chat.gui.autoCompletePlugin.promoteNick('OverRustla');
         var m = m=destiny.chat.onMSG({data:'Check tab completion of GameOfThrows and OverRustle',
             nick:'BetterDGG', features:[]});
-        destiny.chat.gui.push(m);
+        push(m);
     };
 
     bdgg.test3 = function() {
         var m = destiny.chat.onMSG({data:'ChanChamp http://GabeN.com/foo?s=Riperino&p=DESBRO PJSalt',
             nick:'BetterDGG', features:[]});
-        destiny.chat.gui.push(m);
+        push(m);
     };
 
     bdgg.test4 = function() {
         var m = destiny.chat.onMSG({data:'>green ChanChamp http://GabeN.com/foo?s=Riperino&p=DESBRO PJSalt',
             nick:'BetterDGG', features:['flair3']});
-        destiny.chat.gui.push(m);
+        push(m);
     };
 
     bdgg.test.chat.disconnect = function() {
@@ -41,7 +45,7 @@
         count = count || 5;
         for (var i=0; i<count; i++) {
             var m = destiny.chat.onMSG({data:emote, nick:nick, features:features});
-            m && destiny.chat.gui.push(m);
+            push(m);
         }
     };
 
@@ -49,64 +53,64 @@
         var emoticons = bdgg.emoticons.all;
         for (var i=0; i<emoticons.length; i++) {
             var m = destiny.chat.onMSG({data:emoticons[i], nick:'BetterDGG', features:[]});
-            m && destiny.chat.gui.push(m);
+            push(m);
         }
     };
 
     bdgg.test.chat.broadcast = function(str) {
         str = str || 'BetterDGG is now a Tier II subscriber! gifted by 9inevolt OverRustle';
-        destiny.chat.gui.push(destiny.chat.onBROADCAST({data:str}));
+        push(destiny.chat.onBROADCAST({data:str}));
     };
 
     bdgg.test.chat.info = function(str) {
         str = str || "Available commands: /emotes /me /ignore (without arguments to list the nicks ignored) /unignore /highlight (highlights target nicks messages for easier visibility) /unhighlight /maxlines /mute /unmute /subonly /ban /ipban /unban (also unbans ip bans) /timestampformat";
-        destiny.chat.gui.push(new ChatInfoMessage(str));
+        push(new ChatInfoMessage(str));
     };
 
     bdgg.test.chat.status = function(str) {
         str = str || "Connecting to server...";
-        destiny.chat.gui.push(new ChatStatusMessage(str));
+        push(new ChatStatusMessage(str));
     };
 
     bdgg.test.chat.error = function(str) {
         str = str || "Error contacting server";
-        destiny.chat.gui.push(new ChatErrorMessage(str));
+        push(new ChatErrorMessage(str));
     };
 
     bdgg.test.chat.command = function(str) {
         str = str || "BetterDGG muted by Destiny";
-        destiny.chat.gui.push(new ChatCommandMessage(str));
+        push(new ChatCommandMessage(str));
     };
 
     bdgg.test.chat.flair = function(str) {
         var msg = str || "hi there";
-        destiny.chat.gui.push(destiny.chat.onMSG({data:msg, nick:'ILiedAboutCake', features:[]}));
-        destiny.chat.gui.push(destiny.chat.onMSG({data:msg, nick:'mellipelli', features:[]}));
-        destiny.chat.gui.push(destiny.chat.onMSG({data:msg, nick:'Zanshin314', features:[]}));
-        destiny.chat.gui.push(destiny.chat.onMSG({data:msg, nick:'Mannekino', features:[]}));
+        push(destiny.chat.onMSG({data:msg, nick:'ILiedAboutCake', features:[]}));
+        push(destiny.chat.onMSG({data:msg, nick:'mellipelli', features:[]}));
+        push(destiny.chat.onMSG({data:msg, nick:'Zanshin314', features:[]}));
+        push(destiny.chat.onMSG({data:msg, nick:'Mannekino', features:[]}));
     };
 
     bdgg.test.chat.highlight = function(str) {
         var msg = destiny.chat.user.username + " " + (str || "hi there");
-        destiny.chat.gui.push(destiny.chat.onMSG({data:msg, nick:'BetterDGG', features:[]}));
+        push(destiny.chat.onMSG({data:msg, nick:'BetterDGG', features:[]}));
     };
 
     bdgg.test.chat.ignore = function(str) {
         var msg = str || " ^nsfw$ ";
-        destiny.chat.gui.push(destiny.chat.onMSG({data:msg, nick:'BetterDGG', features:[]}));
+        push(destiny.chat.onMSG({data:msg, nick:'BetterDGG', features:[]}));
     };
 
     bdgg.test.chat.self = function(str) {
         var msg = destiny.chat.onMSG({data:str || 'Message from self WhoahDude',
             nick:destiny.chat.user.username, features:destiny.chat.user.features});
-        destiny.chat.gui.push(msg);
+        push(msg);
     };
 
     bdgg.test.chat.inject = function(str) {
         var msg = destiny.chat.onMSG({data:(str || destiny.chat.user.username + ' <script type="text/javascript">' +
                 'alert("hi there");</script><b>hi</b><br>there'),
             nick:'BetterDGG', features:['flair3']});
-        destiny.chat.gui.push(msg);
+        push(msg);
     };
 
     bdgg.test.chat.mention = function(user1, user2, msg) {
@@ -114,8 +118,8 @@
         user1 = user1 || destiny.chat.users[allUsers[0]].username;
         user2 = user2 || destiny.chat.users[allUsers[1]].username;
         msg = msg || "hi there";
-        destiny.chat.gui.push(destiny.chat.onMSG({data:msg + " " + user2, nick:user1, features:[]}));
-        destiny.chat.gui.push(destiny.chat.onMSG({data:msg + " " + user1, nick:user2, features:[]}));
+        push(destiny.chat.onMSG({data:msg + " " + user2, nick:user1, features:[]}));
+        push(destiny.chat.onMSG({data:msg + " " + user1, nick:user2, features:[]}));
     };
 
     // fake injected content in message
@@ -136,49 +140,49 @@
 
     bdgg.test.chat.override = function(str) {
         var msg = str || "SoSad";
-        destiny.chat.gui.push(destiny.chat.onMSG({data:msg, nick:'BetterDGG', features:[]}));
+        push(destiny.chat.onMSG({data:msg, nick:'BetterDGG', features:[]}));
     };
 
     bdgg.test.or.twitch = function(str) {
         var msg = destiny.chat.onMSG({data:str || 'visit http://www.twitch.tv/9inevolt',
             nick:'BetterDGG', features:['flair1']});
-        destiny.chat.gui.push(msg);
+        push(msg);
     };
 
     bdgg.test.or.hitbox = function(str) {
         var msg = destiny.chat.onMSG({data:str || 'visit http://www.hitbox.tv/9inevolt',
             nick:'BetterDGG', features:['flair1']});
-        destiny.chat.gui.push(msg);
+        push(msg);
     };
 
     bdgg.test.or.castamp = function(str) {
         var msg = destiny.chat.onMSG({data:str || 'visit http://castamp.com/live/9inevolt',
             nick:'BetterDGG', features:['flair1']});
-        destiny.chat.gui.push(msg);
+        push(msg);
     };
 
     bdgg.test.or.ustream = function(str) {
         var msg = destiny.chat.onMSG({data:str || 'visit http://www.ustream.tv/embed/6299343',
             nick:'BetterDGG', features:['flair1']});
-        destiny.chat.gui.push(msg);
+        push(msg);
     };
 
     bdgg.test.or.ustream2 = function(str) {
         var msg = destiny.chat.onMSG({data:str || 'visit http://www.ustream.tv/channel/JoeRogan',
             nick:'BetterDGG', features:['flair1']});
-        destiny.chat.gui.push(msg);
+        push(msg);
     };
 
     bdgg.test.or.ustream3 = function(str) {
         var msg = destiny.chat.onMSG({data:str || 'visit http://www.ustream.tv/z',
             nick:'BetterDGG', features:['flair1']});
-        destiny.chat.gui.push(msg);
+        push(msg);
     };
 
     bdgg.test.or.not = function(str) {
         var msg = destiny.chat.onMSG({data:str || 'no http before destiny.gg',
             nick:'BetterDGG', features:['flair1']});
-        destiny.chat.gui.push(msg);
+        push(msg);
     };
 
     bdgg.test.stalk = function(ts, expected) {
@@ -227,6 +231,6 @@
     bdgg.test.subreddit = function(str) {
         var msg = destiny.chat.onMSG({data:str || '/r/destiny! also /r/fallout_shelter, /r/starcraft? maybe /r/games',
             nick:'BetterDGG', features:['flair1']});
-        destiny.chat.gui.push(msg);
+        push(msg);
     };
 }(window.BetterDGG = window.BetterDGG || {}));
