7b71144d6cd73c4259166800536ad851850efd3e
PurpleCox <Christopherflinn@hotmail.com>
2016-03-15 16:32:57 +0000

    Remove useless stuff I missed before the first commit

--- betterdgg/modules/zleftchat.js
+++ betterdgg/modules/zleftchat.js
@@ -1,7 +1,4 @@
 ;(function(bdgg) {
-	//Chat is not resizeable while on the left but can be moved back to the right, resized and moved back
-	//Wait for document to load even though querySelector only returns a value once the page is loaded according to some guy on the internet
-	//$(document).ready(function() {
 		console.log('ready');
 		bdgg.zleft = (function() {
 			var chat, stream
@@ -17,7 +14,7 @@
 					})
 				},
 				leftBoys: function(val) {
-					if (val == true) {
+					if (val) {
 						stream.style.cssFloat = "right";
 						chat.style.cssFloat = "left";
 					} else {
@@ -27,5 +24,4 @@
 				}
 			}
 		})()
-//})
 })(window.BetterDGG = window.BetterDGG || {})
