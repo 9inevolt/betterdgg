0a93f68753daf348804bdadf7c43b2102090518b
PurpleCox <Christopherflinn@hotmail.com>
2016-03-16 20:55:49 +0000

    Fix indentation, remove useless console log

--- betterdgg/modules/leftchat.js
+++ betterdgg/modules/leftchat.js
@@ -1,26 +1,25 @@
 ;(function(bdgg) {
-		console.log('ready');
-		bdgg.left = (function() {
-			var chat, stream
-			return {
-				init: function() {
-					stream = window.parent.document.querySelector("div#stream-panel");
-					chat = window.parent.document.querySelector("div#chat-panel");
-					bdgg.left.leftBoys(bdgg.settings.get('bdgg_left_chat'));
-					bdgg.settings.addObserver(function(key, val) {
-						if (key == 'bdgg_left_chat')
+	bdgg.left = (function() {
+		var chat, stream
+		return {
+			init: function() {
+				stream = window.parent.document.querySelector("div#stream-panel");
+				chat = window.parent.document.querySelector("div#chat-panel");
+				bdgg.left.leftBoys(bdgg.settings.get('bdgg_left_chat'));
+				bdgg.settings.addObserver(function(key, val) {
+					if (key == 'bdgg_left_chat')
 						bdgg.left.leftBoys(val)
-					})
-				},
-				leftBoys: function(val) {
-					if (val) {
-						stream.style.cssFloat = "right";
-						chat.style.cssFloat = "left";
-					} else {
-						stream.style.cssFloat = "left";
-						chat.style.cssFloat = "right";
-					}
+				})
+			},
+			leftBoys: function(val) {
+				if (val) {
+					stream.style.cssFloat = "right";
+					chat.style.cssFloat = "left";
+				} else {
+					stream.style.cssFloat = "left";
+					chat.style.cssFloat = "right";
 				}
 			}
-		})()
+		}
+	})()
 })(window.BetterDGG = window.BetterDGG || {})
