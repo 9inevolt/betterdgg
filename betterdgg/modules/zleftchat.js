ed634942c8a45d54d06822d3b1c2e8feea4c3810
Kirill Voloshin <voloshin.k@husky.neu.edu>
2016-03-18 00:52:57 -0400

    Left Chat

--- betterdgg/modules/zleftchat.js
+++ betterdgg/modules/zleftchat.js
@@ -1,25 +1,88 @@
 ;(function(bdgg) {
-		console.log('ready');
 		bdgg.zleft = (function() {
 			var chat, stream
 			return {
 				init: function() {
-					stream = window.parent.document.querySelector("div#stream-panel");
-					chat = window.parent.document.querySelector("div#chat-panel");
+					stream = $('#stream-panel', parent.document);
+					chat = $('#chat-panel', parent.document);
 					bdgg.zleft.leftBoys(bdgg.settings.get('bdgg_left_chat'));
+					bdgg.zleft.resizeTakeOver();
 					bdgg.settings.addObserver(function(key, val) {
 						if (key == 'bdgg_left_chat')
 							bdgg.zleft.leftBoys(val);
 					})
 				},
 				leftBoys: function(val) {
+					var resizeBar = $('#chat-panel-resize-bar', parent.document);
 					if (val) {
-						stream.style.cssFloat = "right";
-						chat.style.cssFloat = "left";
+						stream.css('float', 'right');
+						chat.css('float', 'left');
+						resizeBar.css({
+							'left': 'auto',
+							'right': 0
+						});
 					} else {
-						stream.style.cssFloat = "left";
-						chat.style.cssFloat = "right";
+						stream.css('float', 'left');
+						chat.css('float', 'right');
+						resizeBar.css({
+							'left': 0,
+							'right': 'auto'
+						});
 					}
+				},
+				resizeTakeOver: function() {
+					var resizeBar = $('#chat-panel-resize-bar', parent.document);
+					var clone = resizeBar.clone();
+					resizeBar.remove();
+					clone.appendTo(chat);
+					resizeBar = $('#chat-panel-resize-bar', parent.document);
+
+					function resizeFrames(w, leftChat) {
+						var chatw = chat.width() + w; 
+						stream.css('width', '-moz-calc(100% - '+ chatw +'px)');
+						stream.css('width', '-webkit-calc(100% - '+ chatw +'px)');
+						stream.css('width', '-o-calc(100% - '+ chatw +'px)');
+						stream.css('width', 'calc(100% - '+ chatw +'px)');
+						chat.css('width', chatw);
+						var side = (leftChat) ? 'right' : 'left';
+						resizeBar.css(side, 0);
+					}
+
+					resizeBar.on('mousedown', function(e) {
+						e.preventDefault();
+						var leftChat = bdgg.settings.get('bdgg_left_chat');
+						var side = (leftChat) ? 'right' : 'left';
+						resizeBar.addClass('active');
+
+						var offsetX = e.clientX;
+						var sx = resizeBar.position().left;
+
+						var overlay = $('<div class="overlay" />');
+						overlay.appendTo(parent.document.body);
+
+
+						var mouseMoveHandler = function(e) {
+							e.preventDefault();
+
+							var offset = (leftChat) ? (-1  * (e.clientX - offsetX)) : (sx + (e.clientX - offsetX));
+							resizeBar.css(side, offset);
+						};
+
+						var mouseUpHandler = function(e) {
+							e.preventDefault();
+
+							resizeBar.removeClass('active');
+							overlay.remove();
+							$(parent.document).unbind('mousemove.chatresize');
+							$(parent.document).unbind('mouseup.chatresize');
+
+							var w = parseInt(resizeBar.css(side)) * -1;
+							resizeFrames(w, leftChat);
+						}
+
+						$(parent.document).on('mousemove.chatresize', mouseMoveHandler);
+						$(parent.document).on('mouseup.chatresize', mouseUpHandler);
+					})
 				}
 			}
 		})()
