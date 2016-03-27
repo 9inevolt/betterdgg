7e0991e0858cc6a6fdf3cebf813daac22ba45861
Kenan Yildirim <kenan@kenany.me>
2016-03-27 01:12:12 -0400

    load settings module first
    Fixes a race condition wherein on some page loads, other modules will load and
    try to get settings before the settings module has loaded.

--- betterdgg/betterdgg.js
+++ betterdgg/betterdgg.js
@@ -1,11 +1,13 @@
+// load settings module before everything else
+window.BetterDGG.settings.init();
+
 for (var module in window.BetterDGG) {
-    if (window.BetterDGG[module].init) {
+    if (module !== 'settings' && window.BetterDGG[module].init) {
         try {
             window.BetterDGG[module].init();
         }
         catch (e) {
             console.log(e)
-            
         }
     }
 }
