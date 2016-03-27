598496da1650385a5dace9d931b6b6a5a1f04117
PurpleCox <Christopherflinn@hotmail.com>
2016-03-27 20:36:28 +0100

    New loading test and fixed ignore
    Fixed ignore and new test loader from SgtMaximum that seems to work

--- betterdgg/betterdgg.js
+++ betterdgg/betterdgg.js
@@ -1,5 +1,11 @@
 for (var module in window.BetterDGG) {
     if (window.BetterDGG[module].init) {
-        window.BetterDGG[module].init();
+        try {
+            window.BetterDGG[module].init();
+        }
+        catch (e) {
+            console.log(e)
+            
+        }
     }
 }
