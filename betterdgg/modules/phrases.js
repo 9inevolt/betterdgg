6d1448dbac5095e5fcd7da3c9643549a0ff74edf
xDashh <Dashh@protonmail.com>
2016-03-31 01:07:45 +0200

    Update to the phrases feature
    Bad phrases are now handled by telling the user what is wrong with the message. The most important mute/ban phrases have been added to the list.

--- betterdgg/modules/phrases.js
+++ betterdgg/modules/phrases.js
@@ -2,24 +2,49 @@
     bdgg.phrases = (function() {
 
         var PHRASES = [ 
+            //old phrases
             /(^|\b)BAR($|\b)/, 
+            /.*?twitter\.com\/antibullyranger.*?/,
+
+            //from addmute or addban
+            /used my safeword/,
+            /just can't nod and smile/,
+            /(^|\s)momo\s.*\sbanned($|\s)|(^|\s)momo banned($|\s)/,
+            /([\s\S]+?)?play([\s\S]+?)?undertale([\s\S]+?)?/,
+            /([\s\S]+?)?dest[il]ny([\s\S]+?)?underta[il]e([\s\S]+?)?/,
+            /!logs Eschkapade/,
+            /\/scyx/,
+            /卐/,
+            /\/r\/bliutwo/,
+            /\/ScYx17/,
+
+            //links
+            /puu\.sh\/iHbP6/, //knock knock
+            /OSCiMbMVDLI/, //wake me up
+            /SUdVyW0tcSo/, //wake me up
+            /dQw4w9WgXcQ/, //rick
+            /puu\.sh\/k0Hki\.jpg/, //knock knock
+            /twitter\.com\/steven__bonnell/,
+            /twitter\.com\/Sc2Destiny/,
+            /tinyurl\.com/,
+
+            //for debugging
             /(^|\b)JUSTATEST($|\b)/
         ];
 
-        function _testPhrase(phrases, line) {
-
-            var abort = false;
+        function _checkMessage(phrases, line) {
 
             for (var i = 0; i < phrases.length; i++) {
-                phraseMatches = line.match(phrases[i]);
+                var phraseMatches = line.match(phrases[i]);
                 if (phraseMatches){
-                        abort = true;
+                    return phrases[i]; //return whatever the message matched so we can give feedback
                 }
             }
 
-            return abort;
+            //no match found, message seems good.
+            return false;
         }
-    
+
         return {
             init: function() {
 
@@ -27,9 +52,12 @@
                 destiny.chat.gui.send = function() {
 
                     if (bdgg.settings.get('bdgg_prohibited_phrase_filter')){
-                        var msg = destiny.chat.gui.input.val()
+                        var message = destiny.chat.gui.input.val();
+                        var blackListMatch = _checkMessage(PHRASES, message);
                         //If the content of the chat input box matches any prohibited phrase, do not call the send function.
-                        if (_testPhrase(PHRASES, msg)) {
+                        if (blackListMatch) {
+                            var errorMessage = new ChatErrorMessage("BBDGG prevented your message from being sent because it matched the following prohibited phrase: " + blackListMatch.toString());
+                            destiny.chat.gui.push(errorMessage);
                             return;
                         }
                     }
