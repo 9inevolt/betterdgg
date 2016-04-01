0824b26889da6400f96a0f4b348859fe26136dd0
PurpleCox <Christopherflinn@hotmail.com>
2016-04-01 23:00:05 +0100

    Turns out FF does support clicking notifications

--- betterdgg/modules/notification.js
+++ betterdgg/modules/notification.js
@@ -6,13 +6,12 @@
                 var bdggChatPRIVMSG = function(data) {
                     var bdggPRIVMSG = fnChatPRIVMSG.apply(this, arguments);
                     var notif;
-                    var isChrome = !!window.chrome && !!window.chrome.webstore;
                     if (bdgg.settings.get('bdgg_Private_Message_Notifications')) {
                         var memes = {
                             body: data.nick + " sent you a private message. Click here to view the chat!",
                             icon: destiny.cdn + '/chat/img/notifyicon.png'
                         };
-                        if (Notification.permission === 'granted' && isChrome){
+                        if (Notification.permission === 'granted'){
                             notif = new Notification("Better Better Destiny.GG", memes);
                             notif.onclick = function() { 
                                 window.focus();
@@ -22,10 +21,7 @@
                                 }
                             };
                             setTimeout(function(){ notif.close(); }, 5000);
-                        } else if (Notification.permission === 'granted' && !isChrome){
-                            notif = new Notification("Better Better Destiny.GG", memes);
-                            setTimeout(function(){ notif.close(); }, 5000);
-                        }
+                        } 
                     }
                     return bdggPRIVMSG;
                 };
