22d477e5491b9fa92c36d83608da74988f934f5b
xDashh <Dashh@protonmail.com>
2016-04-21 01:59:57 +0200

    Allow users to send prohibited phrases via PM.
    Also update phrases.

--- betterdgg/modules/phrases.js
+++ betterdgg/modules/phrases.js
@@ -24,6 +24,8 @@
             /SUdVyW0tcSo/, //wake me up
             /dQw4w9WgXcQ/, //rick
             /puu\.sh\/k0Hki\.jpg/, //knock knock
+            /wbcPKaCSw7k/, //loud
+            /xWObJuha4Ys/, //loud
             /twitter\.com\/steven__bonnell/,
             /twitter\.com\/Sc2Destiny/,
             /tinyurl\.com/,
@@ -51,8 +53,12 @@
                 var fnSendCommand = destiny.chat.gui.send;
                 destiny.chat.gui.send = function() {
 
-                    if (bdgg.settings.get('bdgg_prohibited_phrase_filter')){
-                        var message = destiny.chat.gui.input.val();
+                    var message = destiny.chat.gui.input.val();
+                    //If the message starts with any prefix that would make it a private message, do not check for prohibited phrases.
+                    var isPublicMessage = (message.match(/^\s*(\/w|\/msg|\/message)/i) === null);
+
+                    if (isPublicMessage && bdgg.settings.get('bdgg_prohibited_phrase_filter')){
+
                         var blackListMatch = _checkMessage(PHRASES, message);
                         //If the content of the chat input box matches any prohibited phrase, do not call the send function.
                         if (blackListMatch) {
